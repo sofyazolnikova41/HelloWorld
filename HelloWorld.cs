d83f2f00a9a22050c899d8c7a4ce9c085eadc960
sofyazolnikova41 <97375067+sofyazolnikova41@users.noreply.github.com>
2022-11-20 11:17:49 +0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        //приветствие: 
+        print("Hello, world!");
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
